acaeb5823b2294cbdb7c4cfa1e858ba29ea95eec
HENRIQUE HIDEKI INOUE <henriqueinoue1000@gmail.com>
2025-10-21 08:32:44 -0300

    a

--- index.js
+++ index.js
@@ -3,17 +3,32 @@ const express = require('express');
 const app = express();
 
 const port = 3000;
-app.use(express.json()); //uso de parse json
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-// Handle POST requests to the /submit-form endpoint
 app.post('/submit-form', (req, res) => {
-    // You can now process this data (e.g., save to a database, send an email)
-    res.send('Form submitted successfully!');
+    const { input1, input2, input3,input4 } = req.body;
+
+    var det = (input1 * input4) - (input2 * input3);
+    var resposta;
+    if(det != 0){
+        var input1Inv = input4;
+        var input4Inv = input1;
+        var input2Inv = -input2;
+        var input3Inv = -input3;
+        resposta = `<p>det: ${det}</p>
+                    <p>matriz inversa:<br>
+                    [${input1Inv} ${input2Inv}]<br>
+                    [${input3Inv} ${input4Inv}]</p>`
+    }
+    else{
+        resposta = `<p>det: ${det} <br> não há matriz inversa</p>`;
+    }
+    res.send(resposta);
 });
 
 var server = app.listen( port || process.env.PORT , () =>{
