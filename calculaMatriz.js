094249f13a9f8179ffec68d308fb5e85695289b3
HENRIQUE HIDEKI INOUE <henriqueinoue1000@gmail.com>
2025-10-22 09:37:37 -0300

    bruh

--- calculaMatriz.js
+++ calculaMatriz.js
@@ -2,10 +2,10 @@ function calculaMatriz(numbers)
 {
     var det = (numbers[0] * numbers[3]) - (numbers[1] * numbers[2]);
     if(det != 0){
-        var input1Inv = Number(numbers[3]);
-        var input4Inv = Number(numbers[0]);
-        var input2Inv = -Number(numbers[1]);
-        var input3Inv = -Number(numbers[2]);
+        var input1Inv = Number(numbers[3])/det;
+        var input4Inv = Number(numbers[0])/det;
+        var input2Inv = -Number(numbers[1])/det;
+        var input3Inv = -Number(numbers[2])/det;
         return [true, det, input1Inv, input2Inv, input3Inv, input4Inv]
     }
     return [false, det]
